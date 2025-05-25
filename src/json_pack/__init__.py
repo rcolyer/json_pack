18ad25d8c6b2709c9dc9231e540ffa3f45d73695
Ryan A. Colyer <ryancolyer@yahoo.com>
2025-05-25 12:28:16 -0400

    Version bump

--- src/json_pack/__init__.py
+++ src/json_pack/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2025.03.31'
+__version__ = '2025.04.11'
 
 from ._json_pack import JSLoad,JSLoadStr,JSSave,JSSaveStr
 from ._json_pack import JSLinesLoad,JSLinesLoadStr,JSLinesSave,JSLinesSaveStr
